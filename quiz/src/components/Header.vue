cde64e353d3349afd6963d5eb66a48ccfcfe64bd
keerthana <keerthana.kasthuril@thoughtworks.com>
2021-07-06 18:33:23 +0530

    [KK] Add question counts to navbar

--- quiz/src/components/Header.vue
+++ quiz/src/components/Header.vue
@@ -2,7 +2,9 @@
   <div>
     <b-nav tabs>
       <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
-      <b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>
+      <b-nav-item disabled>Total Questions: 10</b-nav-item>
+      <b-nav-item disabled>Total Answered: {{ numTotal }}</b-nav-item>
+      <b-nav-item disabled>Correct answers: {{ numCorrect }}</b-nav-item>
     </b-nav>
   </div>
 </template>
@@ -10,8 +12,8 @@
 <script>
 export default {
   props: [
-      'numCorrect',
-      'numTotal'
+    'numCorrect',
+    'numTotal'
   ]
 }
 </script>
