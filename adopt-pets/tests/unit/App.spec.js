a4e0635c1437e61beae7da35cafbfabe8b552f7a
keerthana <keerthana.kasthuril@thoughtworks.com>
2021-07-12 20:34:11 +0530

    [KK] Fix lint errors

--- adopt-pets/tests/unit/App.spec.js
+++ adopt-pets/tests/unit/App.spec.js
@@ -1,23 +1,23 @@
 import { mount, createLocalVue } from '@vue/test-utils';
-import VueRouter from "vue-router";
+import VueRouter from 'vue-router';
 import routes from '@/router';
 import BootstrapVue from 'bootstrap-vue';
 import App from '@/App.vue';
 
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
-localVue.use(VueRouter)
+localVue.use(VueRouter);
 
 describe('App component', () => {
   let router;
   beforeEach(() => {
     router = new VueRouter({
       router: routes
-    })
+    });
   });
 
-  it("renders all routes", () => {
-    const wrapper = mount(App, { 
+  it('renders all routes', () => {
+    const wrapper = mount(App, {
       localVue,
       router
     });
@@ -25,9 +25,9 @@ describe('App component', () => {
     const routeLinks = wrapper.findAll('a');
     expect(routeLinks).toHaveLength(3);
   });
-  
-  it("renders Home view when mounted", () => {
-    const wrapper = mount(App, { 
+
+  it('renders Home view when mounted', () => {
+    const wrapper = mount(App, {
       localVue,
       router
     });
@@ -36,28 +36,28 @@ describe('App component', () => {
     expect(wrapper.vm.$route.path).toBe(homePath);
   });
 
-  it("should render Cat view when cats link is clicked", async () => {
-    const wrapper = mount(App, { 
+  it('should render Cat view when cats link is clicked', async () => {
+    const wrapper = mount(App, {
       localVue,
       router
     });
     const catPath = '/cats';
 
-    wrapper.find('#cats').trigger('click')
-    await wrapper.vm.$nextTick()
+    wrapper.find('#cats').trigger('click');
+    await wrapper.vm.$nextTick();
 
     expect(wrapper.vm.$route.path).toBe(catPath);
   });
 
-  it("should render Dog view when dogs link is clicked", async () => {
-    const wrapper = mount(App, { 
+  it('should render Dog view when dogs link is clicked', async () => {
+    const wrapper = mount(App, {
       localVue,
       router
     });
     const dogPath = '/dogs';
 
-    wrapper.find('#dogs').trigger('click')
-    await wrapper.vm.$nextTick()
+    wrapper.find('#dogs').trigger('click');
+    await wrapper.vm.$nextTick();
 
     expect(wrapper.vm.$route.path).toBe(dogPath);
   });
